42fce4009e7f971d6589310bfa4b68c065064166
OtschoP <35844438+OtschoP@users.noreply.github.com>
2025-10-19 06:33:13 +0200

    added calculateRemainingKilometers

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const today = new Date();
 const KM_TARGET = 12761;
 const END_DATE = new Date("2026-10-10");
-let kmLeft = KM_TARGET;
+let kmLeft = "";
 
 function init() {
     renderAll()
@@ -21,4 +21,7 @@ function inputEventListener() {
     });
 }
 
+function calculateRemainingKilometers(km){
+    kmLeft = KM_TARGET - km;
+}
 
